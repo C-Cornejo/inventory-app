9249e8148bc75e7bc09ee1550dc3ee8cf6203f26
Christofer Cornejo <castillo.cornejo94@gmail.com>
2021-04-19 13:05:25 -0600

    added product-row component

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -4,18 +4,18 @@ import { Component, OnInit, Input, Output ,EventEmitter} from '@angular/core';
 import { Product } from './../product.model';
 
 @Component({
-  selector: 'products-list',
+  selector: 'product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
 
-  @Input() productList:Product;
+  @Input() productList:Product[];
   @Output() onProductSelected: EventEmitter<Product>;
   private currentProduct:Product;
 
   constructor() {
-    this.onProductSelected= new EventEmitter();
+    this.onProductSelected = new EventEmitter();
    }
 
    clicked(product:Product):void{
